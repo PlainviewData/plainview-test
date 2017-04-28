877037d127b35c2361d91cfcbdc0de9e4e6fd564
PlainviewData <daniel@plainview.io>
2017-04-28 13:40:24 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var articles;
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+fs.readFile('default_html_contents.json', function(err, data){
+  articles = JSON.parse(data.toString());
+});
+
+// app.use('/', index);
+// app.use('/validPage', users);
+
+app.get('/unchanged_article/simple/:storyId', function(req, res, next){
+  var article = articles[req.params.storyId];
+  res.render('valid_article', {story: article.story, author: article.author, date: article.date, headline: article.headline});
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
