6ee1236ce86f2ce1683f12843f9bf7b749b950a4
dbalagula <db2791@nyu.edu>
2017-05-04 14:48:30 -0400

    Added one temp file to change to check changes

--- app.js
+++ app.js
@@ -12,41 +12,44 @@ var fs = require('fs');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var articles;
-
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-fs.readFile('static_html_contents.json', function(err, data){
-  articles = JSON.parse(data.toString());
-});
+var static_articles;
+var nonstatic_articles;
 
-// app.use('/', index);
-// app.use('/validPage', users);
-
-app.get('/static_article/simple/:storyId', function(req, res, next){
-  var article = articles[req.params.storyId];
-  res.render('valid_article', {story: article.story, author: article.author, date: article.date, headline: article.headline});
-});
-
-app.get('/random_article', function(req, res, next){
+function generateRandomArticle(){
   var story = loremIpsum({count: Math.floor(Math.random() * (30 - 10 + 1)) + 10, units:'sentences' , paragraphLowerBound: 4, paragraphLowerBound: 8});
   var author = loremIpsum({count: Math.floor(Math.random() * (3 - 2 + 1)) + 2, units:'words'});
   var dateObj = new Date();
   var date = (dateObj.getMonth() + 1) + '/' + dateObj.getDate() + '/' +  dateObj.getFullYear();
   var headline = loremIpsum({count: 1, units: 'sentences'});
-  res.render('valid_article', {story: story, author: author, date: date, headline: headline});
+  return {story: article.story, author: article.author, date: article.date, headline: article.headline};
+}
+
+fs.readFile('static_html_contents.json', function(err, data){
+  static_articles = JSON.parse(data.toString());
+});
+
+app.get('/static_article/simple/:storyId', function(req, res, next){
+  var article = static_articles[req.params.storyId];
+  res.render('valid_article', {story: article.story, author: article.author, date: article.date, headline: article.headline});
+});
+
+app.get('/nonstatic_article/simple/:storyId', function(req, res, next){
+  res.render('valid_article', {story: article.story, author: article.author, date: article.date, headline: article.headline});
+});
+
+app.get('/random_article', function(req, res, next){
+  res.render('valid_article', generateRandomArticle());
 });
 
 app.get('/temp_article/:id', function(req, res, next){
@@ -71,20 +74,16 @@ app.post('/temp_article', function(req, res, next){
   res.render('valid_article', content);
 });
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
